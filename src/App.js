0223fedcd74df27e173fc7283c9bfc3b3b755f68
Rickard <rickaard@gmail.com>
2021-05-13 01:22:27 +0200

    switch to styled-components

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+import { Normalize } from 'styled-normalize';
+
+import GlobalStyle from './globalStyles';
 
 import HomePage from './pages/HomePage';
 import SearchPage from './pages/SearchPage';
 
+import Header from './components/Header';
+
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/">
-          <HomePage />
-        </Route>
-        <Route path="/search">
-          <SearchPage />
-        </Route>
-      </Switch>
-    </Router>
+    <ThemeProvider>
+      <Router>
+        <Normalize />
+        <GlobalStyle />
+        <Header />
+        <Switch>
+          <Route exact path="/"><HomePage /></Route>
+          <Route path="/search"><SearchPage /></Route>
+        </Switch>
+      </Router>
+    </ThemeProvider>
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => (
+  <header>
+    header placeholder
+  </header>
+);
+
+export default Header;
