7ad62fbba1e6d4defba7dc9dd142955d3b5c9510
Rickard <rickaard@gmail.com>
2021-05-14 03:02:01 +0200

    add header with logo and links

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import styled from 'styled-components';
 
 function HomePage() {
   return (
-    <div>
+    <Container>
       homepage
-    </div>
+    </Container>
   );
 }
 
 export default HomePage;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+`;
