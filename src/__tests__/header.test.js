602eaf7d621f3d41b1989739dcd2a79f3044f5e7
Rickard <rickaard@gmail.com>
2021-05-16 21:42:01 +0200

    moved BrowserRouter to index.js

--- src/__tests__/header.test.js
+++ src/__tests__/header.test.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import { MemoryRouter, Route } from 'react-router-dom';
+// import userEvent from '@testing-library/user-event';
 import App from '../App';
 
-function setup(initialPaht = '/') {
+function setup(initialPath = '/') {
   let history;
   render(
-    <MemoryRouter initialEntries={[initialPaht]}>
+    <MemoryRouter initialEntries={[initialPath]}>
       <App />
       <Route
         path="*"
@@ -22,10 +23,20 @@ function setup(initialPaht = '/') {
 
 describe('Header', () => {
   test('Logo link leads to homepage when clicked', () => {
-    setup('/search/javascript');
+    // setup('/search/javascript');
+    render(
+      <MemoryRouter initialEntries={['/search/javascript']}>
+        <App />
+      </MemoryRouter>,
+    );
 
-    // const logoLink = screen.getByRole('link', { name: /logo})
+    // userEvent.click(screen.getByRole('link', {name: /search/i }));
 
+    // const logoLink = screen.getByRole('link', {name: /logo.svg/i });
+    // userEvent.click(logoLink);
+
+    // const searchText = screen.getByText(/search page/i);
+    // expect(searchText).toBeInTheDocument();
     screen.debug();
   });
 });
--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 
 import 'normalize.css';
 import './index.css';
 
 // eslint-disable-next-line react/jsx-filename-extension
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <React.StrictMode>
+    <Router>
+      <App />
+    </Router>
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
